231960e3a1aeea4a33103905b78bf6e39f21544c
phher802 <your_email@example.com>
2020-11-18 15:01:34 -0600

    Complete FillRegister method to fill wallet

--- SodaMachine/Wallet.cs
+++ SodaMachine/Wallet.cs
@@ -14,13 +14,33 @@ namespace SodaMachine
         public Wallet()
         {
             Coins = new List<Coin>();
+            
             FillRegister();
         }
         //Member Methods (Can Do)
         //Fills wallet with starting money
         private void FillRegister()
         {
-          
+            for (int i = 0; i < 13; i++) //3.25
+            {
+                Coin quarter = new Quarter();
+                Coins.Add(quarter);
+            }
+
+            for (int i = 0; i < 10; i++) //1.50
+            {
+                Coin dime = new Dime();
+                Coins.Add(dime);
+
+                Coin nickle = new Nickel();
+                Coins.Add(nickle);
+            }
+
+            for (int i = 0; i < 25; i++)
+            {
+                Coin penny = new Penny();
+                Coins.Add(penny);
+            }
         }
     }
 }
