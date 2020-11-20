89b4c0c922053549266c1652e463a67c694fadfe
phher802 <your_email@example.com>
2020-11-20 13:40:17 -0600

    Changed for loop to loop 100x instead of only 10X in GatherCoinsFromWallet

--- SodaMachine/Customer.cs
+++ SodaMachine/Customer.cs
@@ -32,7 +32,7 @@ namespace SodaMachine
             string getCoins;
 
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i++)
             {
 
                 getCoins = UserInterface.CoinSelection(selectedCan, Wallet.Coins);
